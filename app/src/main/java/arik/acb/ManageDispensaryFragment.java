19ab9831fc5848864acc451372131c7933f00974
Norski8 <elementoby@yahoo.com>
2017-08-17 02:20:56 -0700

    Logo

--- app/src/main/java/arik/acb/ManageDispensaryFragment.java
+++ app/src/main/java/arik/acb/ManageDispensaryFragment.java
@@ -1,8 +1,22 @@
 package arik.acb;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by Craig on 8/17/2017.
  */
 
-public class ManageDispensaryFragment {
+public class ManageDispensaryFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.manage_dispensary_layout, container, false);
+
+        return rootView;
+    }
 }
